dae20e1d005a8d6d742c51faba5ff71be8b9853e
Titouan Le Gac <titouanlegac@macbook-pro-de-titouan-2.home>
2025-01-30 18:29:02 +0100

    dev-env init

---
+++ env/install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+XDG_CONFIG_HOME=$HOME/.config
+DEV_ENV=$HOME/dev/env
+
+copy() {
+    echo "Copying $1 to $2"
+    rm -rf $2
+    cp -r $1 $2
+}
+
+copy $DEV_ENV/.zshrc $HOME/.zshrc
+copy $DEV_ENV/.zsh_profile $HOME/.zsh_profile
+copy $DEV_ENV/.config $XDG_CONFIG_HOME
