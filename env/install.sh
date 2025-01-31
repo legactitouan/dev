138945353d4a553944b79f6493602500ae0f1e8d
Titouan Le Gac <titouanlegac@macbook-pro-de-titouan-1.home>
2025-02-01 00:55:05 +0100

    added raycast configuration and merge dotfiles instead of deleting everything to copy

--- env/install.sh
+++ env/install.sh
@@ -4,14 +4,29 @@ XDG_CONFIG_HOME=$HOME/.config
 DEV_ENV=$HOME/dev/env
 
 copy() {
-    echo "Copying $1 to $2"
-    rm -rf $2
-    cp -r $1 $2
+    if [[ -d "$1" ]]; then
+        echo "Copying directory $1 to $2"
+        cp -r "$1" "$2"
+    elif [[ -f "$1" ]]; then
+        echo "Copying file $1 to $2"
+        cp "$1" "$2"
+    else
+        echo "⚠️  Warning: $1 does not exist, skipping..."
+    fi
 }
 
 copy $DEV_ENV/.zshrc $HOME/.zshrc
 copy $DEV_ENV/.zprofile $HOME/.zprofile
 copy $DEV_ENV/.config $XDG_CONFIG_HOME
+
+if [[ -d "$HOME/.config/raycast" ]]; then
+    echo "Raycast exists in home, handling the swap..."
+    rm -rf "$DEV_ENV/.config/raycast/extensions"
+    cp -r "$HOME/.config/raycast/extensions" "$DEV_ENV/.config/raycast/extensions"
+else
+    echo "Raycast does not exist in home, copying from DEV_ENV..."
+    cp -r "$DEV_ENV/.config/raycast" "$HOME/.config/raycast"
+fi
 
 source ~/.zshrc
 source ~/.zprofile
